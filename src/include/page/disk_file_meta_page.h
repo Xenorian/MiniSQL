ed3f74133d0610cfbc1ff7f79f4ec58b1dab09c9
Xenoria <1164903882@qq.com>
2022-05-02 15:31:35 +0800

    1.3 submit wxy

--- src/include/page/disk_file_meta_page.h
+++ src/include/page/disk_file_meta_page.h
@@ -6,6 +6,7 @@
 #include "page/bitmap_page.h"
 
 static constexpr page_id_t MAX_VALID_PAGE_ID = (PAGE_SIZE - 8) / 4 * BitmapPage<PAGE_SIZE>::GetMaxSupportedSize();
+static constexpr page_id_t MAX_VALID_EXTENT_ID = (PAGE_SIZE - 8) / 4;
 
 class DiskFileMetaPage {
 public:
@@ -27,7 +28,7 @@ public:
 public:
   uint32_t num_allocated_pages_{0};
   uint32_t num_extents_{0};   // each extent consists with a bit map and BIT_MAP_SIZE pages
-  uint32_t extent_used_page_[0];
+  uint32_t extent_used_page_[(PAGE_SIZE - 8) / 4];
 };
 
 #endif //MINISQL_DISK_FILE_META_PAGE_H
